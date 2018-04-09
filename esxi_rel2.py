57eb6819f65b43bfd645799b071bbec4b193e9bb
Bulel <34302656+Bulel@users.noreply.github.com>
2018-04-09 11:29:49 +0800

    Update esxi_rel2.py
    1. Parametrize detecting interval
    2. Shutdown local PC after shutdown ESXI
    
    1. 参数化探测间隔
    2. 关闭ESXi后关闭本机

--- esxi_rel2.py
+++ esxi_rel2.py
@@ -2,13 +2,34 @@
 # @function: shutdown esxi after one vm is down
 # @author:   bulel
 
+import win32security
+import win32api
+import win32con
 import paramiko
 import time
 import os
 import sys
 
+#### get the shutdown privilege, (shutdown local pc)
+
+p1 = win32security.LookupPrivilegeValue(None, win32con.SE_SHUTDOWN_NAME)
+htoken = win32security.OpenProcessToken(win32api.GetCurrentProcess(),win32con.TOKEN_ALL_ACCESS)
+
+newstate = [(p1, win32con.SE_PRIVILEGE_ENABLED)]
+
+# Grab the token and adjust its privileges.
+win32security.AdjustTokenPrivileges(htoken, False, newstate)
+
+####
+
+
+print("="*36)
 esxi_host = input("Please input ip of ESXI: \r\n")
+print("="*36)
 vm_host = input("Please input ip of vm: \r\n")
+print("="*36)
+d_time = int(input("Please input interval of detecting: \r\n"))
+print("="*36+"\r\n")
 
 def mlog(log):       
         m=open('esxilog.txt','a')
@@ -63,9 +84,18 @@ while True:
         mlog("poweroffed")
         client.close()
         print("-"*60)
+        
+        time.sleep(3)
+
+        msg = 'Will shutdown the PC after 10 seconds'       
+        win32api.InitiateSystemShutdown(None,msg,10,1,1) 
+        #InitiateSystemShutdown(computerName, message, timeOut, bForceClose, bRebootAfterShutdown)
+        mlog(msg)
+        
         break
+        
     else:
         up = "Host %s is up"%vm_host
         mlog(up)
         print("-"*60)    
-        time.sleep(1800)       #if host is up, sleep 30 minutes, then detect again
+        time.sleep(d_time)       #if host is up, sleep d_time seconds, then detect again
